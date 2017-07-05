38a353c6ce08e62a1337e3c49e456bd95ca0ccaa
Lucas Hart <lukke100@gmail.com>
2017-07-05 10:02:52 -0400

    Reduce armor export to single file
      * Including only BoE reduces the group size significantly; there's no
        need to separate armor types into different import strings.

--- export-everything.sh
+++ export-everything.sh
@@ -8,13 +8,7 @@ mkdir -p "$DIR_EXPORTS"
 
 # Armor
 # =====
-./export-groups.py "output/Armor/Cloaks/"        > "$DIR_EXPORTS/armor-cloaks.txt"
-./export-groups.py "output/Armor/Cloth Armor/"   > "$DIR_EXPORTS/armor-cloth.txt"
-./export-groups.py "output/Armor/Leather Armor/" > "$DIR_EXPORTS/armor-leather.txt"
-./export-groups.py "output/Armor/Mail Armor/"    > "$DIR_EXPORTS/armor-mail.txt"
-./export-groups.py "output/Armor/Plate Armor/"   > "$DIR_EXPORTS/armor-plate.txt"
-./export-groups.py "output/Armor/Shields/"       > "$DIR_EXPORTS/armor-shields.txt"
-./export-groups.py "output/Armor/Shirts/"        > "$DIR_EXPORTS/armor-shirts.txt"
+./export-groups.py "output/Armor/" > "$DIR_EXPORTS/armor.txt"
 
 # Weapons
 # =======
